fc7f1c3d5a7e27fb591883ec6d511628e9910ef2
Ellipec <thomasmckenziek@gmail.com>
2026-08-17 20:27:18 +0930

    Initial commit

---
+++ src/main/java/ellipec/divinerelics/DivineRelics.java
@@ -0,0 +1,18 @@
+package ellipec.divinerelics;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.resources.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DivineRelics implements ModInitializer {
+	public static final String MOD_ID = "divinerelics";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
