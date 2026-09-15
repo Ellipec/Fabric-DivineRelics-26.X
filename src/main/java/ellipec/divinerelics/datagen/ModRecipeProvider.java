48fc983ebc8c9df777b00215e9a3ad809becfc39
Ellipec <thomasmckenziek@gmail.com>
2026-09-15 11:29:32 +0930

    15/09/26 CRAFTING RECIPIES

--- src/main/java/ellipec/divinerelics/datagen/ModRecipeProvider.java
+++ src/main/java/ellipec/divinerelics/datagen/ModRecipeProvider.java
@@ -1,10 +1,8 @@
 package ellipec.divinerelics.datagen;
 
 import ellipec.divinerelics.item.ModItems;
-import ellipec.divinerelics.item.tags.ModTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagsProvider;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
@@ -26,15 +24,116 @@ public class ModRecipeProvider extends FabricRecipeProvider {
 
                 shaped(RecipeCategory.MISC, ModItems.EMBERFANG)
                         .pattern("LBL")
-                        .pattern("LVL")
+                        .pattern("PVP")
                         .pattern("LSL")
+                        .define('V', ModItems.VOLCANIC_CORE)
                         .define('L', Items.LAVA_BUCKET)
                         .define('B', Items.BLAZE_ROD)
-                        .define('V', ModItems.VOLCANIC_CORE)
                         .define('S', Items.NETHERITE_SWORD)
-                        .unlockedBy(getHasName(Items.NETHERITE_SWORD), has(Items.NETHERITE_SWORD))
+                        .define('P', Items.PLAYER_HEAD)
+                        .unlockedBy(getHasName(ModItems.VOLCANIC_CORE), has(ModItems.VOLCANIC_CORE))
                         .group("legendary-weapon")
                         .save(output, "emberfang_sword");
+
+                shaped(RecipeCategory.MISC, ModItems.TEMPEST_EDGE)
+                        .pattern("PEP")
+                        .pattern(" B ")
+                        .pattern(" S ")
+                        .define('E', ModItems.ESSENCE_OF_WIND)
+                        .define('P', Items.PLAYER_HEAD)
+                        .define('B', Items.BREEZE_ROD)
+                        .define('S', Items.NETHERITE_SWORD)
+                        .unlockedBy(getHasName(ModItems.ESSENCE_OF_WIND), has(ModItems.ESSENCE_OF_WIND))
+                        .group("legendary-weapon")
+                        .save(output, "tempest_edge");
+
+                shaped(RecipeCategory.MISC, ModItems.FROSTMOURNE)
+                        .pattern("BFB")
+                        .pattern("PYP")
+                        .pattern("SNS")
+                        .define('F', ModItems.FROZEN_FANG)
+                        .define('Y', ModItems.YETI_HEART)
+                        .define('B', Items.BLUE_ICE)
+                        .define('P', Items.PLAYER_HEAD)
+                        .define('S', Items.POWDER_SNOW_BUCKET)
+                        .define('N', Items.NETHERITE_SWORD)
+                        .unlockedBy(getHasName(ModItems.YETI_HEART), has(ModItems.YETI_HEART))
+                        .group("legendary-weapon")
+                        .save(output, "frostmourne_staff");
+
+                shaped(RecipeCategory.MISC, ModItems.JARNGREIPR)
+                        .pattern("IHI")
+                        .pattern("NBN")
+                        .pattern("PAP")
+                        .define('B', ModItems.BERSERKERS_RELIC)
+                        .define('I', Items.IRON_BLOCK)
+                        .define('N', Items.NETHERITE_INGOT)
+                        .define('P', Items.PLAYER_HEAD)
+                        .define('H', Items.HEAVY_CORE)
+                        .unlockedBy(getHasName(ModItems.BERSERKERS_RELIC), has(ModItems.BERSERKERS_RELIC))
+                        .group("legendary-weapon")
+                        .save(output, "jarngreipr_axe");
+
+                shaped(RecipeCategory.MISC, ModItems.DRAGONS_RUIN)
+                        .pattern("SSS")
+                        .pattern("SES")
+                        .pattern("PNP")
+                        .define('S', ModItems.DRAGON_SCALE)
+                        .define('E', Items.DRAGON_EGG)
+                        .define('P', Items.PLAYER_HEAD)
+                        .define('N', Items.NETHER_STAR)
+                        .unlockedBy(getHasName(ModItems.DRAGON_SCALE), has(ModItems.DRAGON_SCALE))
+                        .group("legendary-weapon")
+                        .save(output, "dragons_ruin_scythe");
+
+                shaped(RecipeCategory.MISC, ModItems.CROWN_OF_THE_ANCIENTS)
+                        .pattern("GSG")
+                        .pattern("GHG")
+                        .pattern("GGG")
+                        .define('S', ModItems.SIGIL_OF_AEGIS)
+                        .define('H', Items.HEAVY_CORE)
+                        .define('G', Items.GOLD_BLOCK)
+                        .unlockedBy(getHasName(ModItems.SIGIL_OF_AEGIS), has(ModItems.SIGIL_OF_AEGIS))
+                        .group("legendary-weapon")
+                        .save(output, "crown_of_the_ancients_helmet");
+
+                shaped(RecipeCategory.MISC, ModItems.SOUL_OF_AEGIS)
+                        .pattern("IAI")
+                        .pattern("IHI")
+                        .pattern("GNG")
+                        .define('A', ModItems.HEART_OF_AEGIS)
+                        .define('I', Items.IRON_BLOCK)
+                        .define('G', Items.GOLD_BLOCK)
+                        .define('H', Items.HEAVY_CORE)
+                        .define('N', Items.NETHERITE_INGOT)
+                        .unlockedBy(getHasName(ModItems.HEART_OF_AEGIS), has(ModItems.HEART_OF_AEGIS))
+                        .group("legendary-weapon")
+                        .save(output, "soul_of_aegis_chestplate");
+
+                shaped(RecipeCategory.MISC, ModItems.AEGIS_RESOLVE)
+                        .pattern("IAI")
+                        .pattern("GHG")
+                        .pattern("N N")
+                        .define('A', ModItems.AEGIS_REMNANT)
+                        .define('H', Items.HEAVY_CORE)
+                        .define('N', Items.NETHERITE_INGOT)
+                        .define('I', Items.IRON_BLOCK)
+                        .define('G', Items.GOLD_BLOCK)
+                        .unlockedBy(getHasName(ModItems.AEGIS_REMNANT), has(ModItems.AEGIS_REMNANT))
+                        .group("legendary-weapon")
+                        .save(output, "aegis_resolve_leggings");
+
+                shaped(RecipeCategory.MISC, ModItems.AEGIS_VALOR)
+                        .pattern(" F ")
+                        .pattern("GHG")
+                        .pattern("N N")
+                        .define('F', ModItems.FRAGMENT_OF_VALOR)
+                        .define('G', Items.GOLD_BLOCK)
+                        .define('H', Items.HEAVY_CORE)
+                        .define('N', Items.NETHERITE_INGOT)
+                        .unlockedBy(getHasName(ModItems.FRAGMENT_OF_VALOR), has(ModItems.FRAGMENT_OF_VALOR))
+                        .group("legendary-weapon")
+                        .save(output, "aegis_valor_boots");
             }
         };
     }
